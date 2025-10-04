88c957943674bcb6d7f7ad76e496fe548d6a486c
kcancurly <karadumankcan@gmail.com>
2025-10-04 22:53:46 +0300

    u

--- gpoanalyzer/main.py
+++ gpoanalyzer/main.py
@@ -119,6 +119,6 @@ def main():
         print(f"CN: {entry.cn}")
         print(f"Path: {entry.gPCFileSysPath}")
         print(f"Functionality Version: {entry.gPCFunctionalityVersion}")
-        smb_conn = pysmbconn.SMBConnection(args.username, args.password, '', "WINTERFELL", is_direct_tcp=True)
+        smb_conn = pysmbconn.SMBConnection(args.username, args.password, '', "", is_direct_tcp=True)
         if smb_conn.connect(args.host, 445):
             print("yay")
